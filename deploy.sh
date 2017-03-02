949e2f3d205bb7301d0b54398afc944dcd574b5a
Christian Genco <christian.genco@gmail.com>
2017-03-01 18:43:36 -0700

    ignore node_modules when deploying

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 aws s3 sync $DIR s3://emcexpresscare.com --acl=public-read \
 --exclude ".git/*" \
+--exclude "node_modules/*" \
 --exclude ".gitignore" \
 --exclude ".DS_Store" \
 --exclude "todo.md"
